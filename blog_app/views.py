55b4b1de287a4aecf80ae0e44414b9880cc4d37d
Linda Busani <busanilinda.tech@gmail.com>
2025-05-02 09:25:57 +0200

    Add files via upload

---
+++ blog_app/views.py
@@ -0,0 +1,92 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .forms import CommentForm
+from .models import Post, Comment
+
+# Create your views here.
+
+def home(request):
+    context = {
+        'posts': Post.objects.all() 
+    }
+    return render(request, 'blog_app/home.html', context)
+ 
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog_app/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+    paginate_by = 3
+
+
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'blog_app/user_posts.html'
+    context_object_name = 'posts'
+    paginate_by = 3
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user 
+        return super().form_valid(form)
+    
+
+def AddCommentView(request, post_id):
+    post = get_object_or_404(Post, id=post_id)  
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post 
+            comment.save()
+            return redirect('post-detail', pk=post.id)  
+    else:
+        form = CommentForm()
+
+    return render(request, 'blog_app/add_comment.html', {'form': form, 'post': post})
+
+    
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user 
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False   
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post 
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+  
+
+def about (request):
+    return render(request, 'blog_app/about.html')
+ 
